0dc3c7d5e9a0cc5f84047d2fd4b6c911de53b8bc
Soumili <soumilimukherjee20@gmail.com>
2024-10-18 15:22:00 +0530

    hero.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import React from 'react'
 import Button from "./Components/Button"
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Header from "./Components/Header"
+import Hero from "./Components/Hero"
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
       
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header/>
+        <Hero/>
          
       </div>
       <ButtonGradient />
